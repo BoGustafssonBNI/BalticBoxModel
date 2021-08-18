476fb12708b8e601ff59d29a35319e6ddecbfa0e
BoGustafssonBNI <bo.gustafsson@su.se>
2021-08-18 13:04:58 +0200

    Initial Commit

---
+++ BalticBoxModel/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BalticBoxModel
+//
+//  Created by Bo Gustafsson on 2021-08-18.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
